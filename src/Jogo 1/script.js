64c8b29093f3f0fc6b1eb8171fe1325d1bd64111
CleberSalustiano <kleberdejesus45@hotmail.com>
2022-08-26 22:32:13 -0300

    fix: sem duas divs dentro de uma

--- src/Jogo 1/script.js
+++ src/Jogo 1/script.js
@@ -6,29 +6,33 @@ for (const fill of fills) {
 	fill.addEventListener("dragstart", dragStart);
 	fill.addEventListener("dragend", dragEnd);
 
-// Loop through empty boxes and add listeners
-for (const empty of empties) {
-	empty.addEventListener("dragover", dragOver);
-	empty.addEventListener("dragenter", dragEnter);
-	empty.addEventListener("dragleave", dragLeave);
-	empty.addEventListener("drop", dragDrop);
-}
-function dragDrop() {
-  if (fill.className === "invisible") {
-    this.className = "empty";
-    this.append(fill);
-  }
-}
+	// Loop through empty boxes and add listeners
+	for (const empty of empties) {
+		empty.addEventListener("dragover", dragOver);
+		empty.addEventListener("dragenter", dragEnter);
+		empty.addEventListener("dragleave", dragLeave);
+		empty.addEventListener("drop", dragDrop);
+	}
+	function dragDrop() {
+		if (fill.className === "invisible") {
+			if (this.className !== "withFill") {
+				this.className = "withFill";
+				this.append(fill);
+			}
+		}
+	}
 }
 // Drag Functions
 
 function dragStart() {
-	this.className += " hold";
-	setTimeout(() => (this.className = "invisible"), 0);
+	if (this.className !== "fillIn") {
+		this.className += " hold";
+		setTimeout(() => (this.className = "invisible"), 0);
+	}
 }
 
 function dragEnd() {
-	this.className = "fill";
+	this.className = "fillIn";
 }
 
 function dragOver(e) {
@@ -37,11 +41,16 @@ function dragOver(e) {
 
 function dragEnter(e) {
 	e.preventDefault();
-	this.className += " hovered";
+	if (this.className !== "withFill") {
+		this.className += " hovered";
+	}
 }
 
 function dragLeave() {
-	this.className = "empty";
+	console.log(this.className.split(" ")[0]);
+	if (this.className.split(" ")[0] == "withFill") {
+		this.className = "withFill";
+	} else {
+		this.className = "empty";
+	}
 }
-
-
