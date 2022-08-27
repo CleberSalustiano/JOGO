a9e2ddbca7ae5be7d9246a5790ad2a93e2aa7724
CleberSalustiano <kleberdejesus45@hotmail.com>
2022-08-27 19:57:48 -0300

    feat: feat: jogo 2 completo

---
+++ src/Jogo 2/Fase 2/script.js
@@ -0,0 +1,82 @@
+const fills = document.querySelectorAll(".fill");
+const empties = document.querySelectorAll(".empty");
+let pontuação;
+
+// Fill listeners
+for (const fill of fills) {
+	fill.addEventListener("dragstart", dragStart);
+	fill.addEventListener("dragend", dragEnd);
+
+	// Loop through empty boxes and add listeners
+	for (const empty of empties) {
+		empty.addEventListener("dragover", dragOver);
+		empty.addEventListener("dragenter", dragEnter);
+		empty.addEventListener("dragleave", dragLeave);
+		empty.addEventListener("drop", dragDrop);
+	}
+
+	function dragDrop() {
+		if (fill.className === "invisible") {
+			if (this.className !== "withFill" && this.children[0] === undefined) {
+				this.className = "withFill";
+				fill.className = "fillIn";
+				this.append(fill);
+			}
+		}
+
+		const withFills = document.querySelectorAll(".withFill");
+		for (const withFill of withFills) {
+			if (withFill.children[0] === undefined) {
+				withFill.className = "empty";
+			}
+		}
+	}
+}
+// Drag Functions
+function dragStart() {
+	if (this.className !== "fillIn") {
+		this.className += " hold";
+		setTimeout(() => (this.className = "invisible"), 0);
+	}
+}
+
+function dragEnd() {
+	this.className = "fill";
+}
+
+function dragOver(e) {
+	e.preventDefault();
+}
+
+function dragEnter(e) {
+	e.preventDefault();
+	if (this.className !== "withFill") {
+		this.className += " hovered";
+	}
+}
+
+function dragLeave() {
+	if (this.className.split(" ")[0] == "withFill") {
+		this.className = "withFill";
+	} else {
+		this.className = "empty";
+	}
+}
+
+function validar() {
+	pontuação = 0;
+	const withFills = document.querySelectorAll(".withFill");
+	for (const withFill of withFills) {
+		if (
+			withFill.children[0] !== undefined &&
+			withFill.children[0].textContent === withFill.id
+		) {
+			pontuação++;
+			withFill.className += " correct"
+		} else {
+			withFill.className += " wrong"
+		}
+	}
+
+	console.log(pontuação);
+}
