663dda025c8473987a2fb1a883602efe7a47171f
CleberSalustiano <kleberdejesus45@hotmail.com>
2022-09-15 16:04:40 -0300

    style: mudança na forma de visualização da % de acerto

--- src/relatorio/script.js
+++ src/relatorio/script.js
@@ -31,7 +31,7 @@ fetch(url, { method: "GET", headers: headers })
           ? "0:" + aluno.J1_tempo
           : "0:0" + aluno.J1_tempo
       }</td>
-      <td>${(+aluno.J1_qtd_acertos / 26) * 100}</td>
+      <td>${((+aluno.J1_qtd_acertos / 26) * 100).toPrecision(3)}</td>
       <td>${
         +aluno.J2_f1_tempo >= 60
           ? +aluno.J2_f1_tempo - Math.trunc(+aluno.J2_f1_tempo / 60) >= 10
@@ -45,7 +45,7 @@ fetch(url, { method: "GET", headers: headers })
           ? "0:" + aluno.J2_f1_tempo
           : "0:0" + aluno.J2_f1_tempo
       }</td>
-      <td>${(+aluno.J2_f1_qtd_acertos / 2) * 100}</td>
+      <td>${((+aluno.J2_f1_qtd_acertos / 2) * 100).toPrecision(3)}</td>
       <td>${
         +aluno.J2_f2_tempo >= 60
           ? +aluno.J2_f2_tempo - Math.trunc(+aluno.J2_f2_tempo / 60) >= 10
@@ -59,7 +59,7 @@ fetch(url, { method: "GET", headers: headers })
           ? "0:" + aluno.J2_f2_tempo
           : "0:0" + aluno.J2_f2_tempo
       }</td>
-      <td>${(+aluno.J2_f2_qtd_acertos / 3) * 100}</td>
+      <td>${((+aluno.J2_f2_qtd_acertos / 3) * 100).toPrecision(3)}</td>
       <td>${+aluno.J2_f3_tempo >= 60
         ? +aluno.J2_f3_tempo - Math.trunc(+aluno.J2_f3_tempo / 60) >= 10
           ? Math.trunc(+aluno.J2_f3_tempo / 60) +
@@ -71,7 +71,7 @@ fetch(url, { method: "GET", headers: headers })
         : +aluno.J2_f3_tempo >= 10
         ? "0:" + aluno.J2_f3_tempo
         : "0:0" + aluno.J2_f3_tempo}</td>
-      <td>${(+aluno.J2_f3_qtd_acertos / 3) * 100}</td>
+      <td>${((+aluno.J2_f3_qtd_acertos / 3) * 100).toPrecision(3)}</td>
       <td>${
         +aluno.J3_tempo >= 60
           ? +aluno.J3_tempo - Math.trunc(+aluno.J3_tempo / 60) >= 10
