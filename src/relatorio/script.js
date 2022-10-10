15044e8a666c8d4c96708caa3fc67400e2fb6c50
CleberSalustiano <kleberdejesus45@hotmail.com>
2022-10-10 07:59:35 -0300

    feat: mudança das palavras, mudança lógica de acertos e script para relatório

--- src/relatorio/script.js
+++ src/relatorio/script.js
@@ -38,83 +38,23 @@ fetch(url, { method: "GET", headers: headers })
       <th>Aluno${count}</th>
       <td>${aluno.idade}</td>
       <td>${
-        +aluno.J1_tempo >= 60
-          ? +aluno.J1_tempo - Math.trunc(+aluno.J1_tempo / 60)*60 >= 10
-            ? Math.trunc(+aluno.J1_tempo / 60) +
-              ":" +
-              (+aluno.J1_tempo -
-              Math.trunc(+aluno.J1_tempo / 60) * 60)
-            : Math.trunc(+aluno.J1_tempo / 60) +
-              ":0" +
-              (+aluno.J1_tempo -
-              Math.trunc(+aluno.J1_tempo / 60) * 60)
-          : +aluno.J1_tempo >= 10
-          ? "0:" + aluno.J1_tempo
-          : "0:0" + aluno.J1_tempo
+        imprimeTempo(aluno.J1_tempo)
       }</td>
       <td>${((+aluno.J1_qtd_acertos / 26) * 100).toPrecision(3)}</td>
       <td>${
-        +aluno.J2_f1_tempo >= 60
-          ? +aluno.J2_f1_tempo - Math.trunc(+aluno.J2_f1_tempo / 60)*60 >= 10
-            ? Math.trunc(+aluno.J2_f1_tempo / 60) +
-              ":" +
-              (+aluno.J2_f1_tempo -
-              Math.trunc(+aluno.J2_f1_tempo / 60)*60)
-            : Math.trunc(+aluno.J2_f1_tempo / 60) +
-              ":0" +
-              (+aluno.J2_f1_tempo -
-              Math.trunc(+aluno.J2_f1_tempo / 60)*60)
-          : +aluno.J2_f1_tempo >= 10
-          ? "0:" + aluno.J2_f1_tempo
-          : "0:0" + aluno.J2_f1_tempo
+        imprimeTempo(aluno.J2_f1_tempo)
       }</td>
-      <td>${((+aluno.J2_f1_qtd_acertos / 2) * 100).toPrecision(3)}</td>
+      <td>${((+aluno.J2_f1_qtd_acertos / 4) * 100).toPrecision(3)}</td>
       <td>${
-        +aluno.J2_f2_tempo >= 60
-          ? +aluno.J2_f2_tempo - Math.trunc(+aluno.J2_f2_tempo / 60)*60 >= 10
-            ? Math.trunc(+aluno.J2_f2_tempo / 60) +
-              ":" +
-              (+aluno.J2_f2_tempo -
-              Math.trunc(+aluno.J2_f2_tempo / 60)*60)
-            : Math.trunc(+aluno.J2_f2_tempo / 60) +
-              ":0" +
-              (+aluno.J2_f2_tempo -
-              Math.trunc(+aluno.J2_f2_tempo / 60) * 60)
-          : +aluno.J2_f2_tempo >= 10
-          ? "0:" + aluno.J2_f2_tempo
-          : "0:0" + aluno.J2_f2_tempo
+        imprimeTempo(aluno.J2_f2_tempo)
       }</td>
-      <td>${((+aluno.J2_f2_qtd_acertos / 3) * 100).toPrecision(3)}</td>
+      <td>${((+aluno.J2_f2_qtd_acertos / 4) * 100).toPrecision(3)}</td>
       <td>${
-        +aluno.J2_f3_tempo >= 60
-          ? +aluno.J2_f3_tempo - Math.trunc(+aluno.J2_f3_tempo / 60)*60 >= 10
-            ? Math.trunc(+aluno.J2_f3_tempo / 60) +
-              ":" +
-             ( +aluno.J2_f3_tempo -
-              Math.trunc(+aluno.J2_f3_tempo / 60)*60)
-            : Math.trunc(+aluno.J2_f3_tempo / 60) +
-              ":0" +
-             ( +aluno.J2_f3_tempo -
-              Math.trunc(+aluno.J2_f3_tempo / 60)*60)
-          : +aluno.J2_f3_tempo >= 10
-          ? "0:" + aluno.J2_f3_tempo
-          : "0:0" + aluno.J2_f3_tempo
+        imprimeTempo(aluno.J2_f3_tempo)
       }</td>
       <td>${((+aluno.J2_f3_qtd_acertos / 3) * 100).toPrecision(3)}</td>
       <td>${
-        +aluno.J3_tempo >= 60
-          ? +aluno.J3_tempo - Math.trunc(+aluno.J3_tempo / 60)*60 >= 10
-            ? Math.trunc(+aluno.J3_tempo / 60) +
-              ":" +
-             ( +aluno.J3_tempo -
-              Math.trunc(+aluno.J3_tempo / 60) *60 )
-            : Math.trunc(+aluno.J3_tempo / 60) +
-              ":0" +
-              (+aluno.J3_tempo -
-              Math.trunc(+aluno.J3_tempo / 60) *60)
-          : +aluno.J3_tempo >= 10
-          ? "0:" + aluno.J3_tempo
-          : "0:0" + aluno.J3_tempo
+        imprimeTempo(aluno.J3_tempo)
       }</td>
     </tr>
     `;
@@ -157,57 +97,31 @@ fetch(url, { method: "GET", headers: headers })
       <th>Total</th>
       <td> ${Math.trunc(idade)} </td>
       <td>
-        ${
-          +J1_tempo >= 60
-            ? +J1_tempo - 60 >= 10
-              ? Math.trunc(+J1_tempo / 60) + ":" + (+J1_tempo - 60)
-              : Math.trunc(+J1_tempo / 60) + ":0" + (+J1_tempo - 60)
-            : +Math.trunc(+J1_tempo) >= 10
-            ? "0:" + Math.trunc(+J1_tempo)
-            : "0:0" + Math.trunc(+J1_tempo)
-        }
+        ${imprimeTempo(J1_tempo)}
       </td>
       <td>${((+J1_qtd_acertos / 26) * 100).toPrecision(3)}</td>
-      <td>${
-        +J2_f1_tempo >= 60
-            ? +J2_f1_tempo - 60 >= 10
-              ? Math.trunc(+J2_f1_tempo / 60) + ":" + (+J2_f1_tempo - 60)
-              : Math.trunc(+J2_f1_tempo / 60) + ":0" + (+J2_f1_tempo - 60)
-            : +Math.trunc(+J2_f1_tempo) >= 10
-            ? "0:" + Math.trunc(+J2_f1_tempo)
-            : "0:0" + Math.trunc(+J2_f1_tempo)
-      }</td>
-      <td>${((+J2_f1_qtd_acertos / 2) * 100).toPrecision(3)}</td>
-      <td>${
-        +J2_f2_tempo >= 60
-            ? +J2_f2_tempo - 60 >= 10
-              ? Math.trunc(+J2_f2_tempo / 60) + ":" + (+J2_f2_tempo - 60)
-              : Math.trunc(+J2_f2_tempo / 60) + ":0" + (+J2_f2_tempo - 60)
-            : +Math.trunc(+J2_f2_tempo) >= 10
-            ? "0:" + Math.trunc(+J2_f2_tempo)
-            : "0:0" + Math.trunc(+J2_f2_tempo)
-      }</td>
-      <td>${((+J2_f2_qtd_acertos / 3) * 100).toPrecision(3)}</td>
-      <td>${
-        +J2_f3_tempo >= 60
-        ? +J2_f3_tempo - 60 >= 10
-          ? Math.trunc(+J2_f3_tempo / 60) + ":" + (+J2_f3_tempo - 60)
-          : Math.trunc(+J2_f3_tempo / 60) + ":0" + (+J2_f3_tempo - 60)
-        : +Math.trunc(+J2_f3_tempo) >= 10
-        ? "0:" + Math.trunc(+J2_f3_tempo)
-        : "0:0" + Math.trunc(+J2_f3_tempo)
-      }</td>
+      <td>${imprimeTempo(J2_f1_tempo)}</td>
+      <td>${((+J2_f1_qtd_acertos / 4) * 100).toPrecision(3)}</td>
+      <td>${imprimeTempo(J2_f2_tempo)}</td>
+      <td>${((+J2_f2_qtd_acertos / 4) * 100).toPrecision(3)}</td>
+      <td>${imprimeTempo(J2_f3_tempo)}</td>
       <td>${((+J2_f3_qtd_acertos / 3) * 100).toPrecision(3)}</td>
-      <td>${
-        +J3_tempo >= 60
-            ? +J3_tempo - 60 >= 10
-              ? Math.trunc(+J3_tempo / 60) + ":" + (+J3_tempo - 60)
-              : Math.trunc(+J3_tempo / 60) + ":0" + (+J3_tempo - 60)
-            : +Math.trunc(+J3_tempo) >= 10
-            ? "0:" + Math.trunc(+J3_tempo)
-            : "0:0" + Math.trunc(+J3_tempo)
-      }</td>
+      <td>${imprimeTempo(J3_tempo)}</td>
     </tr>
   </table>
   `;
   });
+
+const imprimeTempo = (JTempo) => {
+  return +JTempo >= 60
+    ? +JTempo - Math.trunc(+JTempo / 60) * 60 >= 10
+      ? Math.trunc(+JTempo / 60) +
+        ":" +
+        (+JTempo - Math.trunc(+JTempo / 60) * 60)
+      : Math.trunc(+JTempo / 60) +
+        ":0" +
+        (+JTempo - Math.trunc(+JTempo / 60) * 60)
+    : +JTempo >= 10
+    ? "0:" + Math.trunc(+JTempo)
+    : "0:0" + Math.trunc(+JTempo);
+};
