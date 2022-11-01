3688d438026165f6ae43f1f9c4a2946c10a0ff05
CleberSalustiano <kleberdejesus45@hotmail.com>
2022-10-31 21:37:00 -0300

    feat: correções de acordo com o solicitado

--- src/Jogo1/script.js
+++ src/Jogo1/script.js
@@ -1,64 +1,66 @@
 const fills = document.querySelectorAll(".fill");
 const empties = document.querySelectorAll(".empty");
+const coluna3 = document.querySelector(".coluna3");
+const coluna4 = document.querySelector(".coluna4");
 let pontuação;
 
 // Fill listeners
 for (const fill of fills) {
-	fill.addEventListener("dragstart", dragStart);
-	fill.addEventListener("dragend", dragEnd);
+  fill.addEventListener("dragstart", dragStart);
+  fill.addEventListener("dragend", dragEnd);
 
-	// Loop through empty boxes and add listeners
-	for (const empty of empties) {
-		empty.addEventListener("dragover", dragOver);
-		empty.addEventListener("dragenter", dragEnter);
-		empty.addEventListener("dragleave", dragLeave);
-		empty.addEventListener("drop", dragDrop);
-	}
+  // Loop through empty boxes and add listeners
+  for (const empty of empties) {
+    empty.addEventListener("dragover", dragOver);
+    empty.addEventListener("dragenter", dragEnter);
+    empty.addEventListener("dragleave", dragLeave);
+    empty.addEventListener("drop", dragDrop);
+  }
 
-	function dragDrop() {
-		if (fill.className === "invisible") {
-			if (this.className !== "withFill" && this.children[0] === undefined) {
-				this.className = "withFill";
-				fill.className = "fillIn";
-				this.append(fill);
-			}
-		}
+  function dragDrop() {
+    if (fill.className === "invisible") {
+      if (this.className !== "withFill" && this.children[0] === undefined) {
+        this.className = "withFill";
+        fill.className = "fillIn";
+        this.append(fill);
+      }
+    }
 
-		const withFills = document.querySelectorAll(".withFill");
-		for (const withFill of withFills) {
-			if (withFill.children[0] === undefined) {
-				withFill.className = "empty";
-			}
-		}
-	}
+    const withFills = document.querySelectorAll(".withFill");
+    for (const withFill of withFills) {
+      if (withFill.children[0] === undefined) {
+        withFill.className = "empty";
+      }
+    }
+  }
 }
 // Drag Functions
 function dragStart() {
-	if (this.className !== "fillIn") {
-		this.className += " hold";
-		setTimeout(() => (this.className = "invisible"), 0);
-	}
+  if (this.className !== "fillIn") {
+    this.className += " hold";
+    setTimeout(() => (this.className = "invisible"), 0);
+  }
 }
 
 function dragEnd() {
-	this.className = "fill";
+  this.className = "fill";
 }
 
 function dragOver(e) {
-	e.preventDefault();
+  e.preventDefault();
 }
 
 function dragEnter(e) {
-	e.preventDefault();
-	if (this.className !== "withFill") {
-		this.className += " hovered";
-	}
+  e.preventDefault();
+  if (this.className !== "withFill") {
+    this.className += " hovered";
+  }
 }
 
 function dragLeave() {
-	if (this.className.split(" ")[0] == "withFill") {
-		this.className = "withFill";
-	} else {
-		this.className = "empty";
-	}
+  if (this.className.split(" ")[0] == "withFill") {
+    this.className = "withFill";
+  } else {
+    this.className = "empty";
+  }
 }
